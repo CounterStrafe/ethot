d2830a8898dcbd2c3c4b262812d42c5a16fda742
Eric Oswald <eoswald39@gmail.com>
2020-03-04 00:10:26 -0500

    Start eBot connection manager.

--- src/ethot/core.clj
+++ src/ethot/core.clj
@@ -5,13 +5,24 @@
             [clojure.core.async :as async]
             [clojure.string :as str]
             [clj-http.client :as hclient]
+            [clj-http.conn-mgr :as conn-mgr]
             [config.core :refer [env]])
   (:gen-class))
 
 (def state (atom {}))
+(def ebot-cm (conn-mgr/make-reusable-conn-manager {}))
 
 (def apik (:toornament-api-key env))
+(def ebot-admin-user (:ebot-admin-user env))
+(def ebot-admin-pass (:ebot-admin-pass env))
+(def ebot-url (:ebot-url env))
+
+(defn ebot-login []
+  (let [
+    ebot-login-url (str ebot-url "/admin.php/guard/login")
+    post-data {}])
+  (hclient/post ebot-login-url))
 
 (defn -main
   [& args]
-  (println (str "TOORNAMENT API KEY: " apik)))
+  (println (str (hclient/get "http://www.google.com" {:connection-manager ebot-cm}))))
